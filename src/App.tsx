0bd6eef04ba34c6b929b2b822d2d144c1d6b6277
yangyangeeee <sophia_ysy@naver.com>
2026-04-24 17:09:42 +0900

    ✨ Feat: 아티스트 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import "./App.css";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <>
+//       <h1>Hello React</h1>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ArtistPage from "./routes/Artist";
 
 function App() {
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/artist" element={<ArtistPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
