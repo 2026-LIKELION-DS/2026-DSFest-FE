2357daecfc2cdaa1b77a9c5ef7eb1907066b8f16
yangyangeeee <sophia_ysy@naver.com>
2026-04-25 00:25:42 +0900

    💄 Style: 네브바 padding-top 수정

--- src/layouts/AppLayout.style.ts
+++ src/layouts/AppLayout.style.ts
@@ -21,6 +21,4 @@ export const NavWrapper = styled.div`
 export const Main = styled.main<{ $showNavBar: boolean }>`
   flex: 1;
   overflow-y: auto;
-
-  padding-bottom: ${({ $showNavBar }) => ($showNavBar ? "30px" : "0")};
 `;
