2649f469c805ef8c4d2b3e880587582a1290cf0d
2godong <sora050821@gmail.com>
2026-04-25 00:41:49 +0900

    🎀 feat: TimeTable 컴포넌트 완성

---
+++ src/components/Schedule/TimeTableComponent.tsx
@@ -0,0 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import * as S from "../../styles/TimeTable.style";
+
+import CircleIcon from "../../assets/motions/CircleDefault.svg";
+import CircleSelectedIcon from "../../assets/motions/CircleSelected.svg";
+
+interface ScheduleItem {
+  id: number;
+  title: string;
+  time: string;
+  isActive: boolean;
+  link?: string;
+}
+
+interface TimeTableProps {
+  day: string;
+  schedule: ScheduleItem[];
+}
+
+export default function TimeTable({ day, schedule }: TimeTableProps) {
+  const navigate = useNavigate();
+
+  return (
+    <S.Wrapper>
+      <S.DayHeader>{day}</S.DayHeader>
+      <S.Container>
+        <S.Guide>클릭하면 관련 페이지로 이동합니다</S.Guide>
+        <S.Timeline>
+          {schedule.map((item, index) => (
+            <S.Row
+              key={item.id}
+              onClick={() => item.link && navigate(item.link)}
+              style={{ cursor: item.link ? "pointer" : "default" }}
+            >
+              <S.DotWrapper>
+                <S.DotIcon
+                  src={item.isActive ? CircleSelectedIcon : CircleIcon}
+                  alt="circle"
+                />
+                {index < schedule.length - 1 && <S.Line />}
+              </S.DotWrapper>
+              <S.Card $isActive={item.isActive}>
+                <S.Title $isActive={item.isActive}>{item.title}</S.Title>
+                <S.Time $isActive={item.isActive}>{item.time}</S.Time>
+              </S.Card>
+            </S.Row>
+          ))}
+        </S.Timeline>
+      </S.Container>
+    </S.Wrapper>
+  );
+}
--- src/routes/Schedule.tsx
+++ src/routes/Schedule.tsx
@@ -1,7 +1,48 @@
-// import React, { useState } from "react";
 import * as S from "../styles/Schedule.style";
+import TimeTable from "../components/Schedule/TimeTableComponent";
 
-function SchedulePage() {
-  return <S.SchedulePage></S.SchedulePage>;
+const scheduleData = [
+  {
+    id: 1,
+    title: "낮부스",
+    time: "11:00~14:30",
+    isActive: false,
+    link: "/booth",
+  },
+  {
+    id: 2,
+    title: "영화 상영 <빅토리>",
+    time: "15:30~17:30",
+    isActive: true,
+  },
+  {
+    id: 3,
+    title: "밤부스",
+    time: "16:00~19:30",
+    isActive: true,
+    link: "/booth",
+  },
+  { id: 4, title: "덕우존 입장", time: "17:30~", isActive: false },
+  { id: 5, title: "총장님 인사", time: "18:00~18:30", isActive: false },
+  {
+    id: 6,
+    title: "재학생 및 동아리 공연",
+    time: "18:30~20:00",
+    isActive: false,
+  },
+  {
+    id: 7,
+    title: "아티스트 공연",
+    time: "20:30~22:00",
+    isActive: false,
+    link: "/artist",
+  },
+];
+
+export default function SchedulePage() {
+  return (
+    <S.SchedulePage>
+      <TimeTable day="DAY 1" schedule={scheduleData} />
+    </S.SchedulePage>
+  );
 }
-export default SchedulePage;
