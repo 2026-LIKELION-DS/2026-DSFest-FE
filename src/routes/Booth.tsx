4a69735f7c25cda7a2aa287350eadef36a3d483f
unknown <aquahua0409@gmail.com>
2026-04-27 11:49:39 +0900

    🎀 Feat : 부스페이지 낮밤필터 안내말풍선 추가 (#77)

--- src/routes/Booth.tsx
+++ src/routes/Booth.tsx
@@ -13,7 +13,6 @@ import randomIcon from "../assets/Booth/RandomBooth.svg";
 import announceIcon from "../assets/Booth/BoothAnnounce.svg";
 import upIcon from "../assets/Booth/BoothUp.svg";
 import examplePhoto from "../assets/hahyunsang_sample.svg";
-import upWhiteIcon from "../assets/Booth/BoothUpWhite.svg";
 
 const DAYS_DATA = [
   { id: 1, date: "13일", dayOfWeek: "수" },
@@ -170,47 +169,16 @@ const BoothPage: React.FC = () => {
   };
   return (
     <>
-      {showGuide && (
-        <S.OnboardingOverlay onClick={() => setShowGuide(false)}>
-          <S.GuideContainer onClick={(e) => e.stopPropagation()}>
-            {/* 1. 가짜 버튼 위치 잡기 */}
-            <S.WhiteHighlight>
-              <S.FakeHeaderWrapper>
-                <S.HeaderToggleOverlay
-                  style={{
-                    position: "relative",
-                    top: 0,
-                    left: 0,
-                    transform: "none",
-                    margin: 0,
-                    zIndex: 1,
-                    filter: "none",
-                    boxShadow: "none",
-                  }}
-                >
-                  <S.HeaderTimeOption $active={true}>
-                    <img src={daySelected} alt="낮" />
-                    <span>낮</span>
-                  </S.HeaderTimeOption>
-                  <S.HeaderTimeOption $active={false}>
-                    <img src={nightUnselected} alt="밤" />
-                    <span>밤</span>
-                  </S.HeaderTimeOption>
-                </S.HeaderToggleOverlay>
-              </S.FakeHeaderWrapper>
-            </S.WhiteHighlight>
-
-            <S.GuideText>
-              <img src={upWhiteIcon} alt="arrow" />
-              <span>낮과 밤, 원하는 시간대의{"\n"}부스를 확인해 보세요!</span>
-            </S.GuideText>
-
-            <S.CloseGuideBtn onClick={() => setShowGuide(false)}>
-              닫기
-            </S.CloseGuideBtn>
-          </S.GuideContainer>
-        </S.OnboardingOverlay>
-      )}
+      <S.OnboardingOverlay
+        $isVisible={showGuide}
+        onClick={() => setShowGuide(false)}
+      >
+        <S.GuideContainer onClick={() => setShowGuide(false)}>
+          <S.GuideText>
+            <span>원하는 시간대의{"\n"}부스를 확인해 보세요!</span>
+          </S.GuideText>
+        </S.GuideContainer>
+      </S.OnboardingOverlay>
       <S.HeaderToggleOverlay>
         <S.HeaderTimeOption
           $active={!isNight}
