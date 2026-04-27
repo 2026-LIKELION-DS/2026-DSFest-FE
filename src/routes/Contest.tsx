fce12eeee008ec82e27191596325c26f4ee8b642
2godong <sora050821@gmail.com>
2026-04-27 17:56:33 +0900

    🔨 refactor: contest페이지 총학생회 링크 연결

--- src/routes/Contest.tsx
+++ src/routes/Contest.tsx
@@ -13,7 +13,7 @@ export default function ContestPag() {
           // phase="before"
           phase="vote"
           remainingTime="23:59:59"
-          kakaoLink="https://open.kakao.com/..." //총학 카카오톡 링크
+          kakaoLink="https://pf.kakao.com/_gUyQn"
         />
       </S.VoteButtonWrapper>
     </S.ContestPage>
