867a07d0ec2a22d60da1f287c70e4e6429e56bed
2godong <sora050821@gmail.com>
2026-04-28 04:51:00 +0900

    🎀 feat: Header 말풍선 추가

--- src/components/Common/Header.style.ts
+++ src/components/Common/Header.style.ts
@@ -1,4 +1,15 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { typography } from "../../styles/theme";
+
+const setTypo = (key: keyof typeof typography) => {
+  const typo = typography[key];
+  return css`
+    font-family: ${typo.fontFamily};
+    font-weight: ${typo.fontWeight};
+    font-size: ${typo.fontSize};
+    line-height: ${typo.lineHeight};
+  `;
+};
 
 export const Container = styled.header`
   height: 60px;
@@ -30,4 +41,39 @@ export const Subtitle = styled.span`
   font-size: 14px;
   color: ${({ theme }) => theme.colors.fg.subtle};
   font-weight: 700;
+`;
+
+//
+export const StatusBubble = styled.div`
+  ${setTypo("decorationSm")};
+  background-color: #e8eedf;
+  color: #1a3c34;
+  padding: 6px 14px;
+  border-radius: 999px;
+
+  font-size: 18px;
+  white-space: nowrap;
+  font-weight: 400;
+
+  position: absolute;
+  left: 65%;
+  transform: translateX(-50%);
+
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: -8px;
+    top: 50%;
+    transform: translateY(-50%);
+    border-top: 5px solid transparent;
+    border-bottom: 5px solid transparent;
+    border-right: 10px solid #e8eedf;
+  }
+
+  @media (min-width: 468px) or (max-height: 973px) {
+    left: 250px;
+    top: 30%;
+  }
 `;
