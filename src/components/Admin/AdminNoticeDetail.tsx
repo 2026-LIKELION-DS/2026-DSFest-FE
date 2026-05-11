b5b24b619a21bf0e9bbba6c440d898fce8ef2220
김예나 <gimhh770@gmail.com>
2026-05-11 18:37:14 +0900

    Refactor: 디테일 페이지 안전성

--- src/components/Admin/AdminNoticeDetail.tsx
+++ src/components/Admin/AdminNoticeDetail.tsx
@@ -138,8 +138,8 @@ export default function AdminNoticeDetail() {
             <S.Title>{notice.title}</S.Title>
 
             <S.ImageScrollArea>
-              {notice.imageUrls.length > 0 ? (
-                notice.imageUrls.map((imageUrl, index) => (
+              {(notice.imageUrls ?? []).length > 0 ? (
+                (notice.imageUrls ?? []).map((imageUrl, index) => (
                   <S.ImageBox key={`${imageUrl}-${index}`}>
                     <img src={imageUrl} alt={`공지 이미지 ${index + 1}`} />
                   </S.ImageBox>
